8455471a6ecf630d69a5865ea8d88958e007704a
jdpkumar-hub <jdpkumar@gmail.com>
2026-04-09 20:32:30 -0500

    Add files via upload

---
+++ ai-auth-frontend/app/page.js
@@ -0,0 +1,30 @@
+"use client";
+
+import { supabase } from "../lib/supabase";
+
+export default function Home() {
+
+  const loginWithGoogle = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: "http://localhost:3000/dashboard"
+      }
+    });
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>AI DBA Assistant</h1>
+
+      <button onClick={loginWithGoogle} style={{
+        padding: "12px 20px",
+        borderRadius: "8px",
+        border: "1px solid #ddd",
+        cursor: "pointer"
+      }}>
+        🔵 Continue with Google
+      </button>
+    </div>
+  );
+}
